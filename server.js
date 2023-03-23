ee5cc8a9b0d2c8d40833dbbd709bea409b8485c8
david <david.cmcglobal@gmail.com>
2023-03-23 21:51:14 +0700

    init

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
